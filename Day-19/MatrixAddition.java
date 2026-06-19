f96b4aa33340cb26f032054dffd4771dbee16af3
Prakhar Pandey <pandeyprakhar2006pp@gmail.com>
2026-06-19 08:28:55 +0530

    Add files via upload

---
+++ Day-19/MatrixAddition.java
@@ -0,0 +1,61 @@
+//Wap to add matrices
+import java.util.Scanner;
+public class MatrixAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows of matrix A:");
+        int rowsA = sc.nextInt();
+        System.out.println("Enter the number of columns of matrix A:");
+        int colsA = sc.nextInt();
+        System.out.println("Enter the number of rows of matrix B:");
+        int rowsB = sc.nextInt();
+        System.out.println("Enter the number of columns of matrix B:");
+        int colsB = sc.nextInt();
+        
+        if (rowsA != rowsB || colsA != colsB) {
+            System.out.println("Incompatible matrix dimensions so addition cannot be performed.");
+            return;
+        }
+        
+        int[][] matrixA = new int[rowsA][colsA];
+        int[][] matrixB = new int[rowsB][colsB];
+        
+        System.out.println("Enter the elements of the first matrix:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsA; j++) {
+                matrixA[i][j] = sc.nextInt();
+            }
+        }
+        
+        System.out.println("Enter the elements of the second matrix:");
+        for (int i = 0; i < rowsB; i++) {
+            for (int j = 0; j < colsB; j++) {
+                matrixB[i][j] = sc.nextInt();
+            }
+        }
+        
+        int[][] resultMatrix = addMatrices(matrixA, matrixB);
+        
+        System.out.println("Result of Matrix Addition:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsA; j++) {
+                System.out.print(resultMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static int[][] addMatrices(int[][] matrixA, int[][] matrixB) {
+        int rows = matrixA.length;
+        int cols = matrixA[0].length;
+        int[][] resultMatrix = new int[rows][cols];
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                resultMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+        
+        return resultMatrix;
+    }
+}
---
+++ Day-19/TransposeMatrix.java
@@ -0,0 +1,26 @@
+//Wap to transpose matrix
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        for(int i=1;i<matrix.length;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        System.out.println("Transposed Matrix:");
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
